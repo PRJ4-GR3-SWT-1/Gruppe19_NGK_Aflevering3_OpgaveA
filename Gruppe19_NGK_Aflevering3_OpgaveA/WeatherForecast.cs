da2db376923dd559d2ea5aa2eafe2f167f4cb204
Laurits H.G <201905745@post.au.dk>
2021-04-26 14:13:48 +0200

    Add project files.

---
+++ Gruppe19_NGK_Aflevering3_OpgaveA/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Gruppe19_NGK_Aflevering3_OpgaveA
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
